673153b26f68bc9be6387f00b1be4d349adf62e6
SACHINQEEP <patelsachinsp269@gmail.com>
2022-02-20 01:53:48 +0530

    date and workout, running and cycling classes added

--- src/js/model.js
+++ src/js/model.js
@@ -1,12 +1,81 @@
+'use strict';
+
 const formContent = document.querySelector('.form-container');
 const showFocus = document.querySelector('.distance--focus');
 const subForm = document.querySelector('#my-form');
+
+class Workout {
+  // _date = new Date();
+  constructor(distance, duration) {
+    this.distance = distance;
+    this.duration = duration;
+
+    this.getDate();
+  }
+  // Running on February 12
+  getDate() {
+    const day = new Date();
+    console.log(day);
+
+    const option = {
+      month: 'long',
+      day: '2-digit',
+    };
+    const discription = new Intl.DateTimeFormat('en-IN', option).format(day);
+
+    console.log(discription);
+  }
+
+  showDiscription() {
+    this.renderDiscription = `${this.type
+      .toUpperCase()
+      .slice(1)
+      .toLowerCase()} on ${this.getDate}`;
+  }
+}
+
+class Running extends Workout {
+  type = 'Running';
+  constructor(distance, duration, cadence) {
+    super(distance, duration);
+    this.cadence = cadence;
+    this.showDiscription();
+    this._calcPace();
+  }
+
+  _calcPace() {
+    this.pace = this.distance / this.duration;
+    console.log(this.pace);
+  }
+}
+
+class Cycling extends Workout {
+  type = 'Cycling';
+  constructor(distance, duration, elevationGain) {
+    super(distance, duration);
+    this.elevationGain = elevationGain;
+    this.showDiscription();
+    this._calcSpeed();
+  }
+
+  _calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+  }
+}
+
 const showWorkout = document.querySelector('.workout--content');
+const toogleActive = document.getElementById('Activity');
+const distanceEl = document.getElementById('distance');
+const durationEl = document.getElementById('duration');
+const cadenceEl = document.getElementById('cadence');
 
 class App {
   _Map;
   _coord;
   popupEvent;
+  _date = new Date();
+  _workouts = [];
+  _workout;
 
   constructor() {
     this._getLocation();
@@ -69,6 +138,29 @@ class App {
 
   _newWorkout(e) {
     e.preventDefault();
+
+    const allPositive = (...inputValue) => inputValue.every(el => el > 0);
+    const allNumber = (...inputValue) =>
+      inputValue.every(el => Number.isFinite(el));
+
+    const type = toogleActive.value;
+    const distance = +distanceEl.value;
+    const duration = +durationEl.value;
+    let cadence;
+
+    console.log(allPositive(distance, duration), allNumber(distance, duration));
+
+    if (type === 'Running') {
+      const cadence = +cadenceEl.value;
+
+      if (
+        !allNumber(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        // return alert('Input have to be positive numbers!');
+
+        this._workout = new Running(distance, duration, cadence);
+    }
     this._showPopup();
     this._renderWorkout();
   }
